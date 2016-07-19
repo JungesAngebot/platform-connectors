02fb332b7bd5893ce99663f77eab470b9249ffee
pws <patrick.wilmes@bit-lake.com>
2016-07-19 13:59:07 +0200

    - registered blueprints

--- connector/handlers.py
+++ connector/handlers.py
@@ -1,8 +1,11 @@
 from flask import Flask
 
+from connector import youtube, facebook
+
 
 def create_app():
     app = Flask(__name__)
-
+    app.register_blueprint(youtube)
+    app.register_blueprint(facebook)
     return app
 
