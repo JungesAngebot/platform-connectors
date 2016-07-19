6ae4af6a9f4adea8bcdcc2a835980ffc3b169e0e
pws <patrick.wilmes@bit-lake.com>
2016-07-19 13:58:27 +0200

    - added blueprints for facebook and youtube

--- connector/__init__.py
+++ connector/__init__.py
@@ -1,7 +1,11 @@
 import os
 
-from flask import Flask
+from flask import Blueprint
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__)).replace(os.sep + 'connector', '')
 
+
+youtube = Blueprint('youtube', __name__)
+
+facebook = Blueprint('facebook', __name__)
 
