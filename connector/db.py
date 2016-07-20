22dc73671b54f3c0d8da193e4bdc041ca1fa1829
pws <patrick.wilmes@bit-lake.com>
2016-07-20 14:07:20 +0200

    - impl factory method for getting mongo url

--- connector/db.py
+++ connector/db.py
@@ -1,6 +1,12 @@
+import os
 
-def mongo_connection_url():
-    pass
+from config import CONNECTOR_MONGO_DB, ASSET_MONGO_DB
+
+
+def mongo_connection_url(system):
+    if system == 'internal':
+        return CONNECTOR_MONGO_DB if 'CONNECTOR_MONGO_DB' not in os.environ else os.environ['CONNECTOR_MONGO_DB']
+    return ASSET_MONGO_DB if 'ASSET_MONGO_DB' not in os.environ else os.environ['ASSET_MONGO_DB']
 
 
 def connector_registry_collection():
