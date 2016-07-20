92bf3f2ba26fed1ce04561db4930106fdf3eb380
pws <patrick.wilmes@bit-lake.com>
2016-07-20 20:53:05 +0200

    - better default status for mock

--- tests/states_test.py
+++ tests/states_test.py
@@ -14,7 +14,7 @@ class RegistryModelMock(RegistryModel):
         obj.registry_id = registry_id
         obj.video_id = 'videoId'
         obj.category_id = 'categoryId'
-        obj.status = 'status'
+        obj.status = 'notified'
         obj.message = 'message'
         obj.target_platform = 'targetPlatform'
         obj.target_platform_video_id = 'targetPlatformVideoId'
