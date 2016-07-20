5777edb8750ad11db13b1a96a51b7326e00a1358
pws <patrick.wilmes@bit-lake.com>
2016-07-20 20:52:36 +0200

    - creating object (mock)

--- tests/states_test.py
+++ tests/states_test.py
@@ -4,8 +4,22 @@ from connector.db import RegistryModel
 
 
 class RegistryModelMock(RegistryModel):
-    pass
 
+    def _persist(self):
+        pass
+
+    @classmethod
+    def create_from_registry_id(cls, registry_id):
+        obj = cls()
+        obj.registry_id = registry_id
+        obj.video_id = 'videoId'
+        obj.category_id = 'categoryId'
+        obj.status = 'status'
+        obj.message = 'message'
+        obj.target_platform = 'targetPlatform'
+        obj.target_platform_video_id = 'targetPlatformVideoId'
+        obj.mapping_id = 'mappingId'
+        return obj
 
 class TestUploadMechanism(unittest.TestCase):
     pass
