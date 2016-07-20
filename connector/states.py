0c77885b212495cdcf55265ea3bdf69562be20a8
pws <patrick.wilmes@bit-lake.com>
2016-07-20 14:28:28 +0200

    - states draft 1

--- connector/states.py
+++ connector/states.py
@@ -5,3 +5,30 @@ class State(metaclass=ABCMeta):
     def __init__(self):
         self.prev_state = None
         self.following_states = []
+        self.error_state = None
+
+
+class Notified(State):
+    def __init__(self):
+        super().__init__()
+
+
+class DownloadingError(State):
+    def __init__(self):
+        super().__init__()
+
+
+class Uploading(State):
+    def __init__(self):
+        super().__init__()
+
+
+class Downloading(State):
+    def __init__(self):
+        super().__init__()
+        self.prev_state = Notified
+        self.error_state = DownloadingError
+        self.following_states = None
+
+
+
