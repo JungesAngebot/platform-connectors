bcdb7e2ab9dd62210234744cc74375bc379df7b9
pws <patrick.wilmes@bit-lake.com>
2016-07-20 14:21:46 +0200

    - registry model factory

--- connector/db.py
+++ connector/db.py
@@ -1,5 +1,6 @@
 import os
 
+from bson import ObjectId
 from pymongo import MongoClient
 
 from config import CONNECTOR_MONGO_DB, ASSET_MONGO_DB, CONNECTOR_DB, CONNECTOR_REGISTRY, CONNECTOR_MAPPINGS, ASSET_DB, ASSETS
@@ -28,3 +29,35 @@ class MongoDbFactory(object):
     @staticmethod
     def assets_collection():
         return MongoDbFactory._create_mongo_db_client_for_system('external')[ASSET_DB][ASSETS]
+
+
+class RegistryModel(object):
+    def __init__(self):
+        self.registry_id = None
+        self.video_id = None
+        self.category_id = None
+        self.status = None
+        self.message = None
+        self.target_platform = None
+        self.target_platform_video_id = None
+        self.mapping_id = None
+
+    def current_state_of_video(self):
+        pass
+
+    @classmethod
+    def create_from_registry_id(cls, registry_id):
+        collection = MongoDbFactory.connector_registry_collection()
+        try:
+            registry_obj = collection.find_one({'_id': ObjectId(registry_id)})
+            obj = cls()
+            obj.registry_id = registry_id
+            obj.video_id = registry_obj['videoId']
+            obj.category_id = registry_obj['categoryId']
+            obj.status = registry_obj['status']
+            obj.message = registry_obj['message']
+            obj.target_platform = registry_obj['targetPlatform']
+            obj.target_platform_video_id = registry_obj['targetPlatformVideoId']
+            obj.mapping_id = registry_obj['mappingId']
+        except Exception as e:
+            pass
