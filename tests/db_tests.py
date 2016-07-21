2ac95179d18feda43c6ec279790455b39f6e23ba
pws <patrick.wilmes@bit-lake.com>
2016-07-21 10:19:00 +0200

    - next test

--- tests/db_tests.py
+++ tests/db_tests.py
@@ -15,10 +15,23 @@ class CollectionMock(object):
         )
 
 
+class CollectionMockWithoutImageId(object):
+    def find_one(self, query):
+        return dict(
+            name='videoName',
+            text='videoText',
+            tags=[],
+            downloadUrl='downloadUrl',
+        )
+
+
 class DbFactoryMock(MongoDbFactory):
+
+    mock_to_use = CollectionMock
+
     @staticmethod
     def assets_collection():
-        return CollectionMock()
+        return DbFactoryMock.mock_to_use()
 
 
 class VideoModelTest(unittest.TestCase):
@@ -36,7 +49,9 @@ class VideoModelTest(unittest.TestCase):
         self.assertEquals('downloadUrl', model.download_url)
 
     def test_video_without_image_id(self):
-        VideoModel.db_factory = DbFactoryMock
+        factory_mock = DbFactoryMock
+        factory_mock.mock_to_use = CollectionMockWithoutImageId
+        VideoModel.db_factory = factory_mock
 
         model = VideoModel.create_from_video_id('id')
 
@@ -44,7 +59,7 @@ class VideoModelTest(unittest.TestCase):
         self.assertEquals('videoText', model.description)
         self.assertEquals([], model.keywords)
         self.assertEquals('id.mpeg', model.filename)
-        self.assertEquals('some_id', model.image_id)
-        self.assertEquals('fd447058ca5b7cf49fcec33f3476703e', model.hash_code)
+        self.assertEquals(None, model.image_id)
+        self.assertEquals('c8b5d3de41ac85dc57bfede16b147d25', model.hash_code)
         self.assertEquals('downloadUrl', model.download_url)
 
