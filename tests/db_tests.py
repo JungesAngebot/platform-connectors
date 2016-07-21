0bb9eb42fd4152057d9e289e78307b06de5c75c7
pws <patrick.wilmes@bit-lake.com>
2016-07-21 10:48:38 +0200

    - test class for testing the registry model

--- tests/db_tests.py
+++ tests/db_tests.py
@@ -213,3 +213,7 @@ class VideoModelTest(unittest.TestCase):
         model = VideoModel.create_from_video_id('id')
 
         self.assertEquals(['tag 1', 'tag 2', 'tag 3'], model.keywords)
+
+
+class RegistryModelTest(unittest.TestCase):
+    pass
