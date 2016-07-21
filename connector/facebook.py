d35e43f897c7cc561a64f9d5359b475351f7b907
Jan Mischlich <mischlich@accso.de>
2016-07-21 13:13:37 +0200

    - added functions to facebook adapter

--- connector/facebook.py
+++ connector/facebook.py
@@ -64,19 +64,25 @@ def update_video_on_facebook(video: VideoModel, registry: RegistryModel):
     if registry.target_platform_video_id is None or registry.intermediate_state != 'updating':
         raise Exception("Upload not triggered because registry %s is not in correct state" % registry.registry_id)
 
-    if video.video_hash_code == registry.video_hash_code:
+    if video.hash_code == registry.video_hash_code:
         log_info("Metadata of registry entry %s not changed, so no update needed." % registry.registry_id)
         return
 
     mapping = MappingModel.create_from_mapping_id(registry.mapping_id)
     try:
-        get_metadata_url = API_URL + "/" + registry.target_platform_video_id + "?access_token="+str(mapping.target_id)+"&fields=description,content_category,content_tags,title"
+        get_metadata_url = API_URL + "/" + registry.target_platform_video_id + "?access_token=" + str(
+            mapping.target_id) + "&fields=description,content_tags,title"
 
         metadata_result = requests.get(get_metadata_url).json()
 
         video_remote_hash = create_metadata_hash(metadata_result)
 
+        if registry.video_hash_code != video_remote_hash:
+            log_info("Metadata of video %s was changed on facebook. No update allowed." % registry.registry_id)
+            return
+
         body = {
+            'access_token': str(mapping.target_id),
             'name': video.title,
             'description': video.description
         }
@@ -85,25 +91,71 @@ def update_video_on_facebook(video: VideoModel, registry: RegistryModel):
 
         result = requests.post(update_url, body)
 
+        if result.status_code != 200:
+            raise Exception("Invalid response.", result.json())
+
     except Exception as e:
-        raise Exception("Error updating video of registry entry %s to facebook." % registry.registry_id, e)
+        raise Exception("Error updating video of registry entry %s on facebook." % registry.registry_id, e)
 
 
 def unpublish_video_on_facebook(video: VideoModel, registry: RegistryModel):
-    pass
+    """
+
+    Unpublish video on facebook.
+
+    :param video: information about the video to upload
+    :param registry: current status of processing
+
+    """
+    if registry.target_platform_video_id is None or registry.intermediate_state not in ('unpublishing', 'deleting'):
+        raise Exception("Unpublishing not triggered because registry %s is not in correct state" % registry.registry_id)
+
+    mapping = MappingModel.create_from_mapping_id(registry.mapping_id)
+    try:
+
+        body = {
+            'access_token': str(mapping.target_id),
+            'expire_now': 'true',
+        }
+
+        update_url = API_URL + "/" + registry.target_platform_video_id
+
+        result = requests.post(update_url, body)
+
+        if result.status_code != 200:
+            raise Exception("Invalid response.", result.json())
+
+    except Exception as e:
+        raise Exception("Error unpublishing video of registry entry %s on facebook." % registry.registry_id, e)
 
 
 def delete_video_on_facebook(video: VideoModel, registry: RegistryModel):
-    pass
+    """
+
+    We do not delete videos, just unpublish them. So method forward to unpublish_video_on_facebook().
+
+    :param video: information about the video to upload
+    :param registry: current status of processing
+
+    """
+    unpublish_video_on_facebook(video, registry)
+
 
 def create_metadata_hash(metadata):
+    """
+
+    Create hash for metadata read for a video from facebook.
+
+    :param metadata: metadata-response
+    :return: md5-hash of values
+    """
     video_hash_code = hashlib.md5()
     if 'title' in metadata:
         video_hash_code.update(bytes(metadata['title'].encode('UTF-8')))
     if 'description' in metadata:
         video_hash_code.update(bytes(metadata['description'].encode('UTF-8')))
     # Facebook await ids. Add detection later.
-    #if 'content_tags' in metadata:
+    # if 'content_tags' in metadata:
     #    video_hash_code.update(bytes(metadata['content_tags'].encode('UTF-8')))
 
     return video_hash_code.hexdigest()
