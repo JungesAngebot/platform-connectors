f0db99cafbebc0d5eaba783ae8aa9a45500e6905
pws <patrick.wilmes@bit-lake.com>
2016-07-21 12:06:02 +0200

    - hook for image downloading

--- connector/states.py
+++ connector/states.py
@@ -3,7 +3,7 @@ import urllib.request
 
 from commonspy.logging import log_error
 
-from connector.db import VideoModel
+from connector.db import VideoModel, persist_video_image_on_disk
 from connector.platforms import PlatformInteraction
 
 
@@ -45,6 +45,7 @@ class Downloading(object):
             self.registry_model.set_intermediate_state_and_persist('downloading')
             video_model = self.video_model_class.create_from_video_id(self.registry_model.video_id)
             self._download_binaries(video_model.download_url, video_model.filename)
+            persist_video_image_on_disk(video_model)
             self.registry_model.update_video_hash_code(video_model.hash_code)
             self._next_state(video_model)
         except Exception as e:
