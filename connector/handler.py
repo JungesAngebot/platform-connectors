73f4db06df4866997cc77d3ca9c3f11e63d47b52
paddyws <patrick.wilmes@bit-lake.com>
2016-07-24 16:25:26 +0200

    - logging statements

--- connector/handler.py
+++ connector/handler.py
@@ -1,4 +1,4 @@
-from commonspy.logging import log_error
+from commonspy.logging import log_error, log_info
 from flask import Flask, jsonify
 
 from connector import api
@@ -14,18 +14,25 @@ def create_app():
 
 @api.route('/update/<string:registry_id>')
 def update_request(registry_id):
+    log_info('Going to execute update / upload for registry id %s' % registry_id)
     try:
         registry_model = RegistryModel.create_from_registry_id(registry_id)
         if registry_model.status == 'notified':
+            log_info('New video detected. Starting upload workflow.')
             Downloading.create_downloading_state(registry_model).run()
         elif registry_model.status == 'active':
+            log_info('Existing video will be updated.')
             Updating.create_updating_state(registry_model).run()
         elif registry_model.status == 'inactive':
+            log_info('Detected inactive video. Activating it again.')
             Active.create_active_state(registry_model).run()
         elif registry_model.status == 'error':
+            log_info('Previous workflow ended with error. Retrying...')
             if registry_model.intermediate_state == 'downloading' or registry_model.intermediate_state == 'uploading':
+                log_info('Retrying upload.')
                 Downloading.create_downloading_state(registry_model).run()
             elif registry_model.intermediate_state == 'updating':
+                log_info('Retrying updating...')
                 Updating.create_updating_state(registry_model).run()
     except Exception as e:
         log_error(e)
