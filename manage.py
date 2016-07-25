3448fa0ddb10d3541efde6173996d6447d6a33dc
b-rank <benjamin.rank@accso.de>
2016-07-25 15:12:10 +0200

    gradle build for docker image implemented

--- manage.py
+++ manage.py
@@ -1,8 +1,9 @@
-from flask.ext.script import Manager
+from flask.ext.script import Manager, Server
 
 from connector.handler import create_app
 
 manager = Manager(create_app)
 
 if __name__ == '__main__':
+    manager.add_command("runserver", Server(host="0.0.0.0", port=5000))
     manager.run()
