2420239f123914d569a34bec86d45a9ab1f3980a
pws <patrick.wilmes@bit-lake.com>
2016-07-28 10:21:35 +0200

    - logging

--- connector/handler.py
+++ connector/handler.py
@@ -20,21 +20,21 @@ def update_request(registry_id):
     try:
         registry_model = RegistryModel.create_from_registry_id(registry_id)
         if registry_model.status == 'notified':
-            log_info('New video detected. Starting upload workflow.')
+            log_info('New video detected. Starting upload workflow. registry id: %s' % registry_id)
             Downloading.create_downloading_state(registry_model).run()
         elif registry_model.status == 'active':
-            log_info('Existing video will be updated.')
+            log_info('Existing video will be updated. registry id: %s' % registry_id)
             Updating.create_updating_state(registry_model).run()
         elif registry_model.status == 'inactive':
-            log_info('Detected inactive video. Activating it again.')
+            log_info('Detected inactive video. Activating it again. registry id: %s' % registry_id)
             Active.create_active_state(registry_model).run()
         elif registry_model.status == 'error':
-            log_info('Previous workflow ended with error. Retrying...')
+            log_info('Previous workflow ended with error. Retrying... registry id: %s' % registry_id)
             if registry_model.intermediate_state == 'downloading' or registry_model.intermediate_state == 'uploading':
-                log_info('Retrying upload.')
+                log_info('Retrying upload. registry id: %s' % registry_id)
                 Downloading.create_downloading_state(registry_model).run()
             elif registry_model.intermediate_state == 'updating':
-                log_info('Retrying updating...')
+                log_info('Retrying updating... registry id: %s' % registry_id)
                 Updating.create_updating_state(registry_model).run()
     except Exception as e:
         log_error(e)
@@ -49,7 +49,7 @@ def unpublish_request(registry_id):
     try:
         registry_model = RegistryModel.create_from_registry_id(registry_id)
         if registry_model.status == 'active' or registry_model.status == 'error':
-            log_info('Unpublishing video...')
+            log_info('Unpublishing video... registry id: %s' % registry_id)
             Unpublish.create_unpublish_state(registry_model).run()
     except Exception as e:
         log_error(e)
