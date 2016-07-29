df5bff650b22fcb2395280d8c1c9a4bfcc55123c
pws <patrick.wilmes@bit-lake.com>
2016-07-29 20:11:56 +0200

    - made simple progress for download (visible on console)

--- extra/binary_upload.py
+++ extra/binary_upload.py
@@ -2,6 +2,22 @@
 import argparse
 import urllib.request
 
+import sys
+
+
+def reporthook(blocknum, blocksize, totalsize):
+    readsofar = blocknum * blocksize
+    if totalsize > 0:
+        percent = readsofar * 1e2 / totalsize
+        s = "\r%5.1f%% %*d / %d" % (
+            percent, len(str(totalsize)), readsofar, totalsize)
+        sys.stderr.write(s)
+        if readsofar >= totalsize:  # near the end
+            sys.stderr.write("\n")
+    else:  # total size is unknown
+        sys.stderr.write("read %d\n" % (readsofar,))
+
+
 parser = argparse.ArgumentParser()
 
 parser.add_argument('-url')
@@ -9,4 +25,4 @@ parser.add_argument('-url')
 args = parser.parse_args()
 
 if args.url:
-    urllib.request.urlretrieve(args.url, 'binary')
+    urllib.request.urlretrieve(args.url, 'binary', reporthook)
