18e97cfd366ed3a6a8ea445b5d0da95d9d8c442b
pws <patrick.wilmes@bit-lake.com>
2016-07-29 20:15:04 +0200

    - reformat

--- extra/binary_upload.py
+++ extra/binary_upload.py
@@ -9,13 +9,13 @@ def reporthook(block_num, block_size, total_size):
     read_so_far = block_num * block_size
     if total_size > 0:
         percent = read_so_far * 1e2 / total_size
-        s = "\r%5.1f%% %*d / %d" % (
+        s = '\r%5.1f%% %*d / %d' % (
             percent, len(str(total_size)), read_so_far, total_size)
         sys.stderr.write(s)
         if read_so_far >= total_size:
-            sys.stderr.write("\n")
+            sys.stderr.write('\n')
     else:
-        sys.stderr.write("read %d\n" % (read_so_far,))
+        sys.stderr.write('read %d\n' % (read_so_far,))
 
 
 parser = argparse.ArgumentParser()
